1c8433adc0019251a66a68b3f7fdf526d2009386
Amotz Terem <amotzte@gmail.com>
2021-11-26 09:40:23 +0200

    Generic chrome parameters passing

--- gstcefsrc.h
+++ gstcefsrc.h
@@ -37,6 +37,8 @@ struct _GstCefSrc {
   guint64 n_frames;
   gulong cef_work_id;
   gchar *url;
+  gchar *chrome_extra_flags;
+
   gboolean gpu;
   gint chromium_debug_port;
   CefRefPtr<CefBrowser> browser;
