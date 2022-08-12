750797f2c8cb063b8d6b2fe1f574bd3b0130871a
Aiden Jeffrey <aiden.jeffrey@loroco.tech>
2022-08-12 11:44:05 +0100

    cefsrc: Remove duplicated includes

--- gstcefsrc.cc
+++ gstcefsrc.cc
@@ -2,12 +2,6 @@
 #include <iostream>
 #include <sstream>
 
-#include <include/cef_app.h>
-#include <include/cef_client.h>
-#include <include/cef_render_handler.h>
-#include <include/cef_life_span_handler.h>
-#include <include/cef_load_handler.h>
-#include <include/wrapper/cef_helpers.h>
 #include <include/base/cef_bind.h>
 #include <include/base/cef_callback_helpers.h>
 #include <include/wrapper/cef_closure_task.h>
